9d39f46999d572f7af086b174431f701aa59d02b
laurenawilkinson <w016426g@student.staffs.ac.uk>
2020-04-06 17:42:07 +0100

    Add homepage cards

---
+++ src/components/HomeCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="card">
+    <div class="card__header">
+      <div class="card__icon">
+        <i 
+          v-if="card.icon.type == 'material'" 
+          class="material-icons">
+          {{ card.icon.src }}
+        </i>
+        <img v-else :src="card.icon.src" />
+      </div>
+      <h3 class="card__heading">
+        {{ card.title }}
+      </h3>
+    </div>
+    <div class="card__body">
+      {{ card.body }}
+    </div>
+    <div class="card__footer">
+      <router-link 
+        :to="card.button.href" 
+        class="button button--primary">
+        {{ card.button.text }}
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeCard',
+  props: {
+    card: Object
+  }
+}
+</script>
+
+<style scoped>
+.card__header {
+    text-align: center;
+  }
+
+  .card__body {
+    padding-left: 4rem;
+    padding-right: 4rem;
+  }
+
+  .card__footer {
+    text-align: center;
+    margin-bottom: 2rem;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,71 @@
 <template>
   <div class="main-content home">
     <section class="hero-section">
-      
+      <div class="hero-section__heading">
+        <h1>Local Grown Goodness</h1>
+        <p class="subtitle">What more could you ask for?</p>
+      </div>
+    </section>
+    <section class="section card-section">
+      <div class="container">
+        <section class="card-list">
+          <home-card v-for="(card, i) in cards" :key="i" :card="card" />
+        </section>
+      </div>
     </section>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import HomeCard from '@/components/HomeCard.vue'
 
 export default {
   name: "Home",
+  components: {
+    HomeCard
+  },
+  data () {
+    return {
+      cards: [
+        {
+          title: 'Pick-Your-Own Produce',
+          icon: {
+            type: 'material',
+            src: 'explore'
+          },
+          body: "We happily invite you to our farms to hand-pick your own fresh fruits and veggies. Browse our available seasonal produce!",
+          button: {
+            text: 'View Current Produce',
+            href: '/produce'
+          }
+        },
+        {
+          title: 'Organic Opportunities',
+          icon: {
+            type: 'material',
+            src: 'date_range'
+          },
+          body: "There's so much to do here at our farms. From strawberry picking to cider tasting, there's something for everyone.",
+          button: {
+            text: 'Discover Events',
+            href: '/events'
+          }
+        },
+        {
+          title: 'Healthy life? Happy life.',
+          icon: {
+            type: 'material',
+            src: 'explore'
+          },
+          body: "What makes our juices oh so healthy? Why is eating healthy so important, anyway? Kids can learn more in our Kidzone area.",
+          button: {
+            text: 'Enter the Kidzone',
+            href: '/kidzone'
+          }
+        }
+      ]
+    }
+  }
 };
 </script>
